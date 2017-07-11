e675ade8ad2cb8c7a270f58ba9962c88f068da1e
Kenneth Lebu <kenlebu@gmail.com>
2017-07-12 00:16:37 +0300

    Add flask
    Add flask to the project and modify the html files to work as templates

--- app/views.py
+++ app/views.py
@@ -1,6 +1,11 @@
 from flask import render_template
 from app import app
 
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html', username='kennlebu')
+
 @app.route('/login')
 def login():
     return render_template('login.html')
