05cde06cc42b4458a1b6da2934bf091af72ce718
Kenneth Lebu <kenlebu@gmail.com>
2017-07-12 15:53:02 +0300

    Add function for login

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from .views import blueprint
+import os
 
 app = Flask(__name__)
 
 app.register_blueprint(blueprint)
-#app.config.from_object('config')
+
 app.static_folder = 'assets'
+app.secret_key = os.urandom(12)
 app.config["DEBUG"] = True
