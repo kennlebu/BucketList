9df1215a08ae54acf92760ca923d7818e3656be0
Kenneth Lebu <kenlebu@gmail.com>
2017-07-22 12:39:03 +0300

    Add tests for views

--- tests/views_tests.py
+++ tests/views_tests.py
@@ -2,6 +2,7 @@ import unittest
 import app.views
 from app.user import User
 from flask import url_for, Response
+import requests
 
 class ViewsTests(unittest.TestCase):
     """ Contains tests for the views """
@@ -21,6 +22,13 @@ class ViewsTests(unittest.TestCase):
         with app.app.test_request_context():
             self.assertEqual(app.views.logout().status_code, 302)
             self.assertEqual(app.views.logout().location, url_for('blueprint.login'))
+
+    def test_view_bucketlist(self):
+        """ Tests whether page for viewing a bucketlist loads """
+
+        with app.app.test_client() as client:
+            res = client.get('/item?type=new')
+            self.assertEqual(res.location().status_code, 200)
 
 if __name__ == '__main__':
     unittest.main()
